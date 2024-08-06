bbfe1dc8cb24e61c400af094cbcd497099e3d9f1
ankit80042 <149438644+ankit80042@users.noreply.github.com>
2024-08-06 20:14:11 +0530

    Update createtest.sql

--- EBSCustomizations/XXHR/sql/createtest.sql
+++ EBSCustomizations/XXHR/sql/createtest.sql
@@ -1,2 +1,10 @@
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100)
+);
+
+
 INSERT INTO employees (employee_id, first_name, last_name, email)
 VALUES (1, 'John', 'Doe', 'john.doe@example.com');
