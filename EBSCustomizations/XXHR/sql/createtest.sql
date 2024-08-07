642444498d9ad5adecdd0b412bec4747f7d06beb
ankit80042 <149438644+ankit80042@users.noreply.github.com>
2024-08-07 23:04:03 +0530

    Update createtest.sql

--- EBSCustomizations/XXHR/sql/createtest.sql
+++ EBSCustomizations/XXHR/sql/createtest.sql
@@ -7,4 +7,4 @@
 
 
 INSERT INTO employees (employee_id, first_name, last_name, email)
-VALUES (13, 'Ankit', 'Shukla', 'ankit.@example.com');
+VALUES (12, 'Ankit1', 'Shukl1a', 'ankit.@example1.com');
