2f39ce764b1922c493c21647d0e7f2ff4ce56b3a
ankit80042 <149438644+ankit80042@users.noreply.github.com>
2024-11-29 02:47:59 +0530

    Update XXHR_PER_SOCIETIES.sql

--- EBSCustomizations/XXHR/sql/XXHR_PER_SOCIETIES.sql
+++ EBSCustomizations/XXHR/sql/XXHR_PER_SOCIETIES.sql
@@ -1,2 +1,21 @@
---            NAME:   XXHR_PER_SOCIETIES1 
-select * from XXHR.XXHR_PER_SOCIETIES;
+INSERT INTO XXHR.XXHR_PER_SOCIETIES (
+    SOCIETY_ID, 
+    CODE, 
+    DATE_START, 
+    DATE_END, 
+    SUBS_AMOUNT, 
+    SUBS_PERIOD, 
+    SUBS_TOTAL, 
+    SUBS_HOLD
+) VALUES (
+    1485,              -- New society ID
+    'XXHR02',          -- New code
+    TO_DATE('01-JAN-2024', 'DD-MON-YYYY'),  -- Start date
+    TO_DATE('31-DEC-2024', 'DD-MON-YYYY'),  -- End date
+    150,               -- Subscription amount
+    12,                -- Subscription period
+    1800,              -- Subscription total
+    150                -- Subscription hold
+);
+
+COMMIT;
