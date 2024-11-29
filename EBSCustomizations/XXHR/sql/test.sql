22c963f88583a24ab2fe2af3c856030608111150
ankit80042 <149438644+ankit80042@users.noreply.github.com>
2024-11-29 13:55:54 +0530

    Update test.sql

--- EBSCustomizations/XXHR/sql/test.sql
+++ EBSCustomizations/XXHR/sql/test.sql
@@ -1,14 +1,17 @@
+DEFINE var1 = 100
+DEFINE var2 = 200
+DEFINE var3 = 300
+DEFINE var4 = 'Y'
+
 DECLARE
-  -- Declare variables with new names (var1, var2, var3, var4)
-  v1 NUMBER := &1; -- Variable 1 (Responsibility ID)
-  v2 NUMBER := &2; -- Variable 2 (Application ID)
-  v3 NUMBER := &3; -- Variable 3 (Login ID) **Declare v3 with value**
-  v4 CHAR(1) := '&4'; -- Variable 4 (Error Flag)
+    v1 NUMBER := &var1;
+    v2 NUMBER := &var2;
+    v3 NUMBER := &var3;
+    v4 CHAR(1) := '&var4';
 BEGIN
-  -- Display the values of the new variables
-  DBMS_OUTPUT.PUT_LINE('Variable 1 (Responsibility ID): ' || v1);
-  DBMS_OUTPUT.PUT_LINE('Variable 2 (Application ID): ' || v2);
-  DBMS_OUTPUT.PUT_LINE('Variable 3 (Login ID): ' || v3);
-  DBMS_OUTPUT.PUT_LINE('Variable 4 (Error Flag): ' || v4);
+    DBMS_OUTPUT.PUT_LINE('Variable 1 (Responsibility ID): ' || v1);
+    DBMS_OUTPUT.PUT_LINE('Variable 2 (Application ID): ' || v2);
+    DBMS_OUTPUT.PUT_LINE('Variable 3 (Login ID): ' || v3);
+    DBMS_OUTPUT.PUT_LINE('Variable 4 (Error Flag): ' || v4);
 END;
 /
