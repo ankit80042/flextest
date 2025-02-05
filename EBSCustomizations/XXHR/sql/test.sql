e816c810d047292480664a704c67c32e9240d290
ankit80042 <149438644+ankit80042@users.noreply.github.com>
2025-02-05 23:56:18 +0530

    Update test.sql

--- EBSCustomizations/XXHR/sql/test.sql
+++ EBSCustomizations/XXHR/sql/test.sql
@@ -1,12 +1,1 @@
-DECLARE
-    v1 NUMBER := &var1;
-    v2 NUMBER := &var2;
-    v3 NUMBER := &var3;
-    v4 CHAR(1) := '&var4';
-BEGIN
-    DBMS_OUTPUT.PUT_LINE('Variable 1 (Responsibility ID): ' || v1);
-    DBMS_OUTPUT.PUT_LINE('Variable 2 (Application ID): ' || v2);
-    DBMS_OUTPUT.PUT_LINE('Variable 3 (Login ID): ' || v3);
-    DBMS_OUTPUT.PUT_LINE('Variable 4 (Error Flag): ' || v4);
-END;
-/
+-- absjcbsjcbsjcbsjb
