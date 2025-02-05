005239d44fa314e8bcd0b003e56a52f89260d889
ankit80042 <149438644+ankit80042@users.noreply.github.com>
2025-02-06 02:24:18 +0530

    Update t.sql

--- EBSCustomizations/XXHR/sql/t.sql
+++ EBSCustomizations/XXHR/sql/t.sql
@@ -1,3 +1,3 @@
--- tester hfyytyfhyyjuvjvjvjbjvychtgchtgcbbhbkskcklkjilcslcisllsiyytkjkbkbskb
+-- tester hfyytyfhyyjuvjvjvjbjvychtgchtgcbbhbkskcklkjilcslcisllsiyytkjkbkbskbnkjsbkjsb
 
 -- RAISE_APPLICATION_ERROR(-20001, 'Deployment failed intentionally.');
