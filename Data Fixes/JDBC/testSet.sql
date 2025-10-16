09b619929de363af2c2ad622c37eed6a010b2239
ankit80042 <149438644+ankit80042@users.noreply.github.com>
2025-10-16 21:29:09 +0530

    Update testSet.sql

--- Data Fixes/JDBC/testSet.sql
+++ Data Fixes/JDBC/testSet.sql
@@ -1,4 +1,23 @@
-ALTER SESSION SET TIMED_STATISTICS=TRUE;
-SET DEFINE OFF;
---- Vr ----
-SELECT * FROM hr.COUNTRIES WHERE COUNTRY_ID LIKE'US&A%';
+-- Create a test table
+IF OBJECT_ID('tempdb..#TestGo') IS NOT NULL
+    DROP TABLE #TestGo;
+GO
+
+CREATE TABLE #TestGo (
+    ID INT IDENTITY(1,1),
+    Name VARCHAR(50)
+);
+GO
+
+-- Insert some sample data
+INSERT INTO #TestGo (Name) VALUES ('Ankit');
+INSERT INTO #TestGo (Name) VALUES ('Shukla');
+GO
+
+-- Select data to confirm inserts
+SELECT * FROM #TestGo;
+GO
+
+-- Drop the table to clean up
+DROP TABLE #TestGo;
+GO
